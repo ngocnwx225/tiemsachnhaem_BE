876e9eecfad94993bd33e71de2cd88cd7287487d
tbtrann1912 <tranbaotrann04@gmail.com>
2025-05-27 15:43:40 +0700

    disable admin require

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const adminAuth = require('../middleware/adminAuth');
+// const adminAuth = require('../middleware/adminAuth');
 
-router.get('/', adminAuth, userController.getAllUsers);
-router.get('/:id', adminAuth, userController.getUserById);
-router.put('/:id', adminAuth, userController.updateUser);
-router.delete('/:id', adminAuth, userController.deleteUser);
+router.get('/', userController.getAllUsers);
+router.get('/:id', userController.getUserById);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router; 
